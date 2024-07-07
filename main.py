cdfa315217788294adbaa38809cecac61b3daa7e
Jan Simon Schmitt <mail+support@jan-schmitt.eu>
2024-07-07 17:03:43 +0200

    delteted comment

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ import time
 ip = input()
 
 def get_size(bytes, suffix="B"):
-    """
-    Scale bytes to its proper format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
         if bytes < factor:
