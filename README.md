20ee7204bb445046de6c745409f676d7643cf186
bonbon08 <mail@jan-schmitt.eu>
2024-11-02 22:20:14 +0100

    improved a little bit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # resource_sender
 a simple script to send the pc recources to a external esp8266 with a display with the Display_Server firmware
+## Start
+```bash
+cargo run (ip)
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::UdpSocket;
 use std::thread;
 use std::time::Duration;
+use std::env;
 use sysinfo::System;
 
 fn get_size(bytes: u64) -> String {
@@ -26,22 +27,23 @@ fn send(message1: &str, message2: &str, ip: &str) -> std::io::Result<()> {
     let blank2 = "0§1                ";
     
     socket.send_to(blank1.as_bytes(), &addr)?;
-    socket.send_to(blank2.as_bytes(), &addr)?;
     socket.send_to(message1.as_bytes(), &addr)?;
+    socket.send_to(blank2.as_bytes(), &addr)?;
     socket.send_to(message2.as_bytes(), &addr)?;
     
     Ok(())
 }
 
 fn main() {
-    let ip = "127.0.0.1";  // Replace with the actual IP address
+    let mut args: Vec<String> = env::args().collect();
+    let ip = args[1].as_mut_str();  
 
     loop {
         let mut sys = System::new_all();
         sys.refresh_all();
         
         let cpu = format!("{:.2}%", sys.global_cpu_info().cpu_usage());
-        let name = format!("{:.2}", sys.global_cpu_info().name());
+        let name = format!("{}", get_size(sys.free_swap()));
         let ram = format!("{}/{}", get_size(sys.used_memory()), get_size(sys.total_memory()));
         
         let message1 = format!("0§0{} {}", cpu, name);
